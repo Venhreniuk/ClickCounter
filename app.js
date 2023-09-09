c0ecc055612e32de80516abb5b02cb70bacc18c0
Paolo <89611182+Venhreniuk@users.noreply.github.com>
2023-09-09 20:13:11 +0000

    commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const passedValue = document.getElementById('passed-value')
+const saveValue = document.getElementById('save-el')
+let counter = 0
+
+function income() {
+    counter++
+    passedValue.innerText = counter
+}
+
+function outcome() {
+    if (counter > 0) {
+        counter--
+        passedValue.innerText = counter
+        if (counter === 0) {
+            minusButton.disabled = true
+        }
+    }
+}
+
+function save() {
+let countStr = counter + ' - ';
+saveValue.textContent += countStr;
+}
